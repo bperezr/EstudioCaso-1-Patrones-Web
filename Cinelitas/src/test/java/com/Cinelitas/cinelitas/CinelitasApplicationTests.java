d9be1c3e50c99b1513d3a0df98185bfa2b3a5883
Bryan Perez <bperezr@outlook.com>
2023-03-09 18:21:21 -0600

    Initial commit
    Test

---
+++ Cinelitas/src/test/java/com/Cinelitas/cinelitas/CinelitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cinelitas.cinelitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinelitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
